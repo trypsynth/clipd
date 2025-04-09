a1c22aa90e96030cce1d6b5e286066393f8ed3f3
Quin <TheQuinbox@gmail.com>
2025-04-08 18:31:10 -0600

    clean up client, add gitignore.

--- cmd/clipd/main.go
+++ cmd/clipd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -22,24 +21,20 @@ type ClipboardRequest struct {
 func main() {
 	configFile, err := os.Open("config.json")
 	if err != nil {
-		log.Fatalf("Error opening config file:", err)
+		log.Fatal(err)
 	}
 	defer configFile.Close()
 	var config Config
-	decoder := json.NewDecoder(configFile)
-	err = decoder.Decode(&config)
-	if err != nil {
-		log.Fatalf("Error decoding config file:", err)
+	if err := json.NewDecoder(configFile).Decode(&config); err != nil {
+		log.Fatal(err)
 	}
 	serverAddress := fmt.Sprintf("%s:%d", config.ServerIP, config.ServerPort)
-	var inputData bytes.Buffer
-	_, err = io.Copy(&inputData, os.Stdin)
+	inputData, err := io.ReadAll(os.Stdin)
 	if err != nil {
-		log.Fatalf("Error reading input: %v\n", err)
+		log.Fatal(err)
 	}
-	err = sendToClipboardServer(serverAddress, inputData.String())
-	if err != nil {
-		log.Fatalf("Error sending data to clipboard server: %v\n", err)
+	if err := sendToClipboardServer(serverAddress, string(inputData)); err != nil {
+		log.Fatal(err)
 	}
 }
 
@@ -54,13 +49,11 @@ func sendToClipboardServer(address, data string) error {
 		return fmt.Errorf("failed to connect to server: %v", err)
 	}
 	defer conn.Close()
-	_, err = conn.Write(jsonData)
-	if err != nil {
+	if _, err := conn.Write(jsonData); err != nil {
 		return fmt.Errorf("error writing to server: %v", err)
 	}
 	buf := make([]byte, 1024)
-	_, err = conn.Read(buf)
-	if err != nil && err != io.EOF {
+	if _, err := conn.Read(buf); err != nil && err != io.EOF {
 		return fmt.Errorf("error reading response from server: %v", err)
 	}
 	return nil
